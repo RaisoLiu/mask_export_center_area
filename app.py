d24aa1b8dcf534ea197e96a43a630010a40ddc14
raiso <rainsoon717@gmail.com>
2023-08-11 14:16:50 +0800

    version 0.0.2 obj design

--- app.py
+++ app.py
@@ -1,39 +1,47 @@
-import cv2
-import numpy as np
+
+
+# app.py
 import plotly.graph_objects as go
-import gradio as gr
-from video_analysis import process_video
 import plotly.subplots as sp
+import gradio as gr
 
+from video_analysis import VideoAnalyzer
 
-def plot_area(results):
-    fig = go.Figure()
+class Plotter:
+    @staticmethod
+    def plot_area(results):
+        fig = go.Figure()
 
-    for color, data in results.items():
-        fig.add_trace(go.Scatter(x=list(range(len(data['area']))), y=data['area'], mode='lines', name=f'{color} area'))
-    
-    fig.show()
+        for color_obj in results.values():
+            fig.add_trace(go.Scatter(x=list(range(len(color_obj.area))), y=color_obj.area, mode='lines', name=f'{color_obj.name} area'))
 
-def plot_coordinates(results):
-    fig = sp.make_subplots(rows=4, cols=1, subplot_titles=list(results.keys()))
-   
+        fig.show()
 
-    row_col = [(1, 1), (2, 1), (3, 1), (4, 1)]
-    for idx, (color, data) in enumerate(results.items()):
-        r, c = row_col[idx]
-        fig.add_trace(go.Scatter(x=list(range(len(data['x']))), y=data['x'], mode='lines', name=f'{color} x'), r, c)
-        fig.add_trace(go.Scatter(x=list(range(len(data['y']))), y=data['y'], mode='lines', name=f'{color} y'), r, c)
+    @staticmethod
+    def plot_coordinates(results):
+        fig = sp.make_subplots(rows=4, cols=1, subplot_titles=list(results.keys()))
 
-    fig.update_layout(title_text="XY-coordinates of the colors over time")
+        row_col = [(1, 1), (2, 1), (3, 1), (4, 1)]
+        for idx, color_obj in enumerate(results.values()):
+            r, c = row_col[idx]
+            fig.add_trace(go.Scatter(x=list(range(len(color_obj.x))), y=color_obj.x, mode='lines', name=f'{color_obj.name} x'), r, c)
+            fig.add_trace(go.Scatter(x=list(range(len(color_obj.y))), y=color_obj.y, mode='lines', name=f'{color_obj.name} y'), r, c)
 
-    fig.show()
-
-
+        fig.update_layout(title_text="XY-coordinates of the colors over time")
+        fig.show()
 
 def interface(input_file):
-    results = process_video(input_file.name)
-    plot_area(results)
-    plot_coordinates(results)
+    object_defs = {
+        'R_leg': (242, 110, 176),
+        'L_leg': (250, 230, 250),
+        'R_hand': (143, 209, 66),
+        'L_hand': (133, 237, 148),
+    }
+
+    analyzer = VideoAnalyzer(object_defs)
+    results = analyzer.process_video(input_file.name)
+    Plotter.plot_area(results)
+    Plotter.plot_coordinates(results)
 
 inputs = gr.inputs.File(label="Upload a video")
 outputs = gr.outputs.Textbox()
--- video_analysis.py
+++ video_analysis.py
@@ -1,59 +1,57 @@
-# 定義四種顏色的範圍
-objects = {
-    'R_leg': (242, 110, 176),
-    'L_leg': (250, 230, 250),
-    'R_hand': (143, 209, 66),
-    'L_hand': (133, 237, 148),
-}
-tolerance = 20
-
+# video_analysis.py
 import cv2
 import numpy as np
-import plotly.graph_objects as go
-import gradio as gr
-def process_video(input_file):
-    cap = cv2.VideoCapture(input_file)
 
-    results = {color: {'x': [], 'y': [], 'area': []} for color in objects}
+class ColorObject:
+    tolerance = 20
 
-    while cap.isOpened():
-        ret, frame = cap.read()
-        if not ret:
-            break
-        frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+    def __init__(self, name, color):
+        self.name = name
+        self.color = color
+        self.x = []
+        self.y = []
+        self.area = []
 
-        for name, target_color in objects.items():
-            mask = np.zeros_like(frame[:,:,0])
-            lower_bound = np.array([target_color[0] - tolerance, target_color[1] - tolerance, target_color[2] - tolerance])
-            upper_bound = np.array([target_color[0] + tolerance, target_color[1] + tolerance, target_color[2] + tolerance])
-            within_range = cv2.inRange(frame, lower_bound, upper_bound)
-            mask[within_range > 0] = 255
+    def process_frame(self, frame):
+        mask = np.zeros_like(frame[:, :, 0])
+        lower_bound = np.array([self.color[0] - ColorObject.tolerance, 
+                                self.color[1] - ColorObject.tolerance, 
+                                self.color[2] - ColorObject.tolerance])
+        upper_bound = np.array([self.color[0] + ColorObject.tolerance, 
+                                self.color[1] + ColorObject.tolerance, 
+                                self.color[2] + ColorObject.tolerance])
+        within_range = cv2.inRange(frame, lower_bound, upper_bound)
+        mask[within_range > 0] = 255
 
+        output = cv2.connectedComponentsWithStats(mask, 8, cv2.CV_32S)
+        num_labels, labels, stats, centroids = output
 
-            output = cv2.connectedComponentsWithStats(mask, 8, cv2.CV_32S)
-            num_labels, labels, stats, centroids = output
+        if num_labels > 1:
+            areas = [stats[i, cv2.CC_STAT_AREA] for i in range(1, num_labels)]
+            max_label = np.argmax(areas)
+            self.x.append(centroids[max_label + 1][0])
+            self.y.append(centroids[max_label + 1][1])
+            self.area.append(areas[max_label])
+        else:
+            self.x.append(None)
+            self.y.append(None)
+            self.area.append(None)
 
-            
-            # 跳過 index 0，因為它是背景
+class VideoAnalyzer:
+    def __init__(self, object_defs):
+        self.objects = {name: ColorObject(name, color) for name, color in object_defs.items()}
 
+    def process_video(self, input_file):
+        cap = cv2.VideoCapture(input_file)
 
-            if num_labels > 1:
-                areas = []
+        while cap.isOpened():
+            ret, frame = cap.read()
+            if not ret:
+                break
+            frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
 
-                # print(num_labels, centroids)
-                for i in range(1, num_labels):
-                    area = stats[i, cv2.CC_STAT_AREA]
+            for obj in self.objects.values():
+                obj.process_frame(frame)
 
-                    areas.append(area)
-
-                max_label = np.argmax(areas)
-                results[name]['x'].append(centroids[max_label+1][0])
-                results[name]['y'].append(centroids[max_label+1][1])
-                results[name]['area'].append(areas[max_label])
-            else:
-                results[name]['x'].append(None)
-                results[name]['y'].append(None)
-                results[name]['area'].append(None)
-
-    cap.release()
-    return results
+        cap.release()
+        return self.objects
